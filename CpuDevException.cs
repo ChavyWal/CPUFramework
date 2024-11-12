3c622deaac0ed7830dfe5d5cc434a57ca11aa790
chavywal <wchavyw@gmail.com>
2024-11-12 13:47:07 -0500

    hw #41

---
+++ CpuDevException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUFramework
+{
+    public class CpuDevException : Exception
+    {
+        public CpuDevException(string? message, Exception? inerexception):base(message, inerexception)
+        {
+
+        }
+    }
+}
--- bizObject.cs
+++ bizObject.cs
@@ -5,22 +5,33 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CPUFramework
 {
-    public class bizObject
+    public class bizObject : INotifyPropertyChanged
     {
         string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = "";
-        string _primarykeyname = ""; string _primarykeyparamname = "";
+        string _primarykeyname = ""; string _primarykeyparamname = "";string _typename = "";
         DataTable _datatable = new();
-        public bizObject(string tablename)
+        List<PropertyInfo> _properties = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bizObject()
         {
-            _tablename = tablename;
-            _getsproc = tablename + "Get";
-            _updatesproc = tablename + "Update";
-            _deletesproc = tablename + "Delete";
-            _primarykeyname = tablename + "Id";
+            Type t = this.GetType();
+            _typename = t.Name;
+            _tablename = _typename;
+            if (_tablename.ToLower().StartsWith("biz")) { _tablename = _tablename.Substring(3); }
+            _getsproc = _tablename + "Get";
+            _updatesproc = _tablename + "Update";
+            _deletesproc = _tablename + "Delete";
+            _primarykeyname = _tablename + "Id";
             _primarykeyparamname = "@" + _primarykeyname;
+            _properties = t.GetProperties().ToList<PropertyInfo>();
         }
 
         public DataTable Load(int primarykeyvalue)
@@ -29,17 +40,70 @@ namespace CPUFramework
             SqlCommand cmd = SQLUtility.GetSqlCommand(_getsproc);
             SQLUtility.Setparamvalue(cmd, _primarykeyparamname, primarykeyvalue);
             dt = SQLUtility.GetDataTable(cmd);
+            if (dt.Rows.Count > 0)
+            {
+                LoadProps(dt.Rows[0]);
+            }
             _datatable = dt;
             return dt;
         }
 
-        public void Delete(DataTable dtpresident)
+        private void LoadProps(DataRow dr)
         {
-            int id = (int)dtpresident.Rows[0][_primarykeyname];
+            foreach (DataColumn col in dr.Table.Columns)
+            {
+                SetProp(col.ColumnName, dr[col.ColumnName]);
+            }
+        }
+
+        public void Save()
+        {
+            SqlCommand cmd = SQLUtility.GetSqlCommand(_updatesproc);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                var prop = GetProp(param.ParameterName, true, false);
+                if (prop != null)
+                {
+                    object? val = prop.GetValue(this);
+                    if (val == null) { val = DBNull.Value; }
+                    param.Value = val;
+                }
+            }
+            SQLUtility.ExecuteSQL(cmd);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                if (param.Direction == ParameterDirection.InputOutput)
+                {
+                    SetProp(param.ParameterName, param.Value);
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
             SqlCommand cmd = SQLUtility.GetSqlCommand(_deletesproc);
             SQLUtility.Setparamvalue(cmd, _primarykeyparamname, id);
             SQLUtility.ExecuteSQL(cmd);
         }
+        public void Delete(DataTable dtpresident)
+        {
+            int id = (int)dtpresident.Rows[0][_primarykeyname];
+            this.Delete(id);
+        }
+
+        public void Delete()
+        {
+            PropertyInfo? prop = GetProp(_primarykeyname, true, false);
+            if (prop != null)
+            {
+                object? id = prop.GetValue(this);
+                if (id != null)
+                {
+                    this.Delete((int)id);
+                }
+            }
+        }
+
         public void Save(DataTable datatable)
         {
             if (datatable.Rows.Count == 0)
@@ -48,6 +112,41 @@ namespace CPUFramework
             }
             DataRow d = datatable.Rows[0];
             SQLUtility.SaveDateRow(d, _updatesproc);
+        }
+
+        private PropertyInfo? GetProp(string propname, bool forread, bool forwrite)
+        {
+            propname = propname.ToLower();
+            if (propname.StartsWith("@")) { propname = propname.Substring(1); }
+            PropertyInfo? prop = _properties.FirstOrDefault(p =>
+            p.Name.ToLower() == propname
+            && (forread == false || p.CanRead == true)
+            && (forwrite == false || p.CanWrite == true)
+            );
+            return prop;
+        }
+
+        private void SetProp(string propname, object? value)
+        {
+            var prop = GetProp(propname, false, true);
+            if (prop != null)
+            {
+                if (value == DBNull.Value) { value = null; }
+                try
+                {
+                    prop.SetValue(this, value);
+                }
+                catch(Exception ex)
+                {
+                    string msg = $"{_typename}.{prop.Name} is being set to {value?.ToString()} and that is the wrong data type. {ex.Message}";
+                    throw new CpuDevException(msg, ex);
+                }
+            }
+        }
+
+        protected void InvokePropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
     }
 }
